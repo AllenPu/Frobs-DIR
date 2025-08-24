7af9e4d05bfefa1449c4f5c0fa820695624151a2
ruizhipu <18651885620@163.com>
2025-08-24 03:58:03 -0400

    add earm up

--- AgeDB-DIR/train.py
+++ AgeDB-DIR/train.py
@@ -6,6 +6,7 @@ import pandas as pd
 from collections import defaultdict
 from scipy.stats import gmean
 
+import torch
 import torch.nn as nn
 import torch.backends.cudnn as cudnn
 from torch.utils.data import DataLoader
@@ -16,9 +17,22 @@ from utils import *
 
 import os
 os.environ["KMP_WARNINGS"] = "FALSE"
-
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 
 
+
+
+
+
+def warm_up_one_epoch(model, train_loader, opt):
+    for idx, (x, y) in enumerate(train_loader):
+        x,y = x.to(device), y.to(device)
+        y_pred = model(x)
+        loss = torch.nn.functional.mse_loss(y, y_pred)
+        opt.zero_grad()
+        loss.backward()
+        opt.step()
+    return model
