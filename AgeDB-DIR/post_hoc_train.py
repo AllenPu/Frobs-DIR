0e6950a096ef9ab13d3d5beda6863c341adacee9
AllenPu <18651885620@163.com>
2025-09-09 18:54:45 -0400

    add train epoch fro linear net train and sft

--- AgeDB-DIR/post_hoc_train.py
+++ AgeDB-DIR/post_hoc_train.py
@@ -5,6 +5,7 @@ from utils import cal_per_label_Frob
 import numpy as np
 from torch.utils.data import DataLoader, TensorDataset
 from tqdm import tqdm
+import torch.optim as optim
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -13,7 +14,10 @@ def post_hoc_train_one_epoch(models, loaders, opts, train_labels, maj_shot, epoc
     #
     model_regression, model_linear = models
     train_loader, val_loader = loaders
-    opt_regression, opt_linear = opts
+    _, opt_linear = opts
+    #
+    opt_regression = optim.Adam(model_regression.parameters(), lr=5e-5, weight_decay=1e-5)
+    #
     regression_epoch, linear_epoch = epochs
     # first calculate the prototypes
     #proto = cal_prototype(model, train_loader)
