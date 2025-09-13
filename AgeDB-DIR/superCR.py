82486187381d87f7233277bb70df100dfa63df6e
ruizhipu <18651885620@163.com>
2025-09-13 00:27:35 -0400

    superCR

--- AgeDB-DIR/superCR.py
+++ AgeDB-DIR/superCR.py
@@ -49,7 +49,7 @@ def get_model(model_name='last.pth', regressor_name='./regressor.pth',  norm=Fal
     #
     regressor = torch.load(regressor_name)
     model.regressor.load_state_dict(regressor['state_dict'])
-    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3,
+    optimizer = torch.optim.SGD(model.parameters(), lr=1e-3,
                                 momentum=0.9, weight_decay=1e-4)
     return model, optimizer
 
