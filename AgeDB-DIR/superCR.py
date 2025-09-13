a952a33c1aa68f491dbd8392596b59f569e7a994
ruizhipu <18651885620@163.com>
2025-09-13 00:31:23 -0400

    superCR

--- AgeDB-DIR/superCR.py
+++ AgeDB-DIR/superCR.py
@@ -1,6 +1,14 @@
 import torch
 from collections import OrderedDict
 from resnet import *
+from train import load_datasets
+import argparse
+from utils import cal_per_label_MAE, cal_per_label_Frob
+
+parser = argparse.ArgumentParser('argument for training')
+parser.add_argument('--dataset', type=str, default='agedb', choices=['agedb'], help='dataset name')
+parser.add_argument('--data_dir', type=str, default='/home/rpu2/scratch/data/imbalanced-regression/agedb-dir/data', help='data directory')
+parser.add_argument('--batch_size', type=int, default=128)
 
 
 
@@ -60,3 +68,37 @@ if __name__ == '__main__':
     regressor_name = paths + 'regressor.pth'
     model = get_model(model_name, regressor_name)
     print('model loaded')
+    #######
+    args = parser.parse_args()
+    train_loader, val_loader, test_loader, train_labels, diff_shots = load_datasets(args)
+    maj_shot, med_shot, few_shot = diff_shots
+    #######
+    per_label_MAE_train = cal_per_label_MAE(model, train_loader)
+    print('===============train key MAE============='+'\n')
+    k_train = [k for k in per_label_MAE_train.keys()]
+    print(k_train + '\n')
+    v_train = [per_label_MAE_train[k] for k in per_label_MAE_train.keys()]
+    print(v_train + '\n')
+    print('===============train MAE============='+'\n')
+    per_label_MAE_test = cal_per_label_MAE(model, test_loader)
+    print('===============test key MAE============='+'\n')
+    k_test = [k for k in per_label_MAE_test.keys()]
+    print(k_test + '\n')
+    v_test = [per_label_MAE_test[k] for k in per_label_MAE_test.keys()]
+    print(v_test + '\n')
+    print('===============test MAE============='+'\n')
+    #
+    per_label_Frobs_train = cal_per_label_Frob(model, train_loader)
+    per_label_Frobs_test = cal_per_label_Frob(model test_loader)
+    k_frobs_train = [k for k in per_label_Frobs_train.keys()]
+    k_frobs_test = [k for k in per_label_Frobs_test.keys()]
+    v_frobs_train = [per_label_Frobs_train[k] for k in per_label_Frobs_train.keys()]
+    v_frobs_test = [per_label_Frobs_train[k] for k in per_label_Frobs_test.keys()]
+    print('===============train frobs key============='+'\n')
+    print(k_frobs_train + '\n')
+    print('===============train frobs============='+'\n')
+    print(v_frobs_train + '\n')
+    print('===============test frobs key============='+'\n')
+    print(k_frobs_test + '\n')
+    print('===============test frobs============='+'\n')
+    print(v_frobs_test + '\n')
