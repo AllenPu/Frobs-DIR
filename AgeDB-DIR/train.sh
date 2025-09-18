530271f27da7f4c8ccfde45aa315f35d978b39d4
ruizhipu <18651885620@163.com>
2025-09-18 02:47:18 -0400

    the train.py to train bmse

--- AgeDB-DIR/train.sh
+++ AgeDB-DIR/train.sh
@@ -8,7 +8,7 @@
 #    done
 #done
 #####################
-for name in 'MSE_LDS'; do
+for name in 'bmse'; do
     for e in 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40; do
         for ee in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15; do
         jobs_name='model_name'_${name}_'regression_epoch'_${e}_'linear_epoch'_${ee}
