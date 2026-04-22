f6dc864df98b518c11edfcfceca1326fabb95ae6
AllenPu <18651885620@163.com>
2026-04-22 21:30:52 +0800

    remove the fast sort

--- AgeDB-DIR/utils.py
+++ AgeDB-DIR/utils.py
@@ -5,7 +5,7 @@ import logging
 import numpy as np
 from scipy.ndimage import gaussian_filter1d
 from scipy.signal.windows import triang
-from fast_soft_sort.pytorch_ops import soft_rank, soft_sort
+#from fast_soft_sort.pytorch_ops import soft_rank, soft_sort
 import pandas as pd
 
 
