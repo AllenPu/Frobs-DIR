61cb8bf8d0e40f3010629978fe1f62f12b829524
AllenPu <18651885620@163.com>
2026-04-24 01:51:24 +0800

    revise

--- AgeDB-DIR/debug.py
+++ AgeDB-DIR/debug.py
@@ -2,7 +2,7 @@ from resnet import *
 from model_repo import *
 
 
-model_name = 'ConR'
+model_name = 'RankSim'
 model_path = build_model_from(model_name)
 model = Regression()
 ckpt = torch.load(model_path)
